ff3a81d8425dfeba2757b53005d4ba2453075cef
Aryan kumar <5325425aryan@gmail.com>
2026-06-18 12:58:19 +0530

    day 16 uploaded

---
+++ day 16/Ques2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+
+    scanf("%d",&n);
+
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+
+    int maxCount=0, element;
+
+    for(int i=0;i<n;i++)
+     {
+        int count=1;
+
+        for(int j=i+1;j<n;j++) 
+        {
+            if(arr[i]==arr[j])
+                count++;
+        }
+
+        if(count>maxCount)
+         {
+            maxCount=count;
+            element=arr[i];
+        }
+    }
+
+    printf("%d",element);
+
+    return 0;
+}
