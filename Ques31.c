e706a34cdc72f5eb2aeb3c5cc9703ff4635817ad
Aryan kumar <5325425aryan@gmail.com>
2026-08-03 13:51:56 +0530

    uploaded day 31

---
+++ Ques31.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main() {
+    int r1, c1, r2, c2;
+    int a[10][10], b[10][10], sum[10][10], product[10][10];
+
+    printf("Enter rows and columns of first matrix: ");
+    scanf("%d %d", &r1, &c1);
+
+    printf("Enter rows and columns of second matrix: ");
+    scanf("%d %d", &r2, &c2);
+
+    printf("\nEnter elements of first matrix:\n");
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("\nEnter elements of second matrix:\n");
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    if (r1 == r2 && c1 == c2) {
+        printf("\nAddition of matrices:\n");
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
+                sum[i][j] = a[i][j] + b[i][j];
+                printf("%d ", sum[i][j]);
+            }
+            printf("\n");
+        }
+    } else {
+        printf("\nAddition is not possible because the matrices are of different sizes.\n");
+    }
+
+    if (c1 == r2) {
+        printf("\nMultiplication of matrices:\n");
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c2; j++) {
+                product[i][j] = 0;
+                for (int k = 0; k < c1; k++) {
+                    product[i][j] += a[i][k] * b[k][j];
+                }
+                printf("%d ", product[i][j]);
+            }
+            printf("\n");
+        }
+    } else {
+        printf("\nMultiplication is not possible because columns of first matrix and rows of second matrix are different.\n");
+    }
+
+    return 0;
+}
